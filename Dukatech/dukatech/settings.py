e401f40772b13ca414ec4baa7d3c69a594c2ef9d
Samuel Maiko <sammymaiko721@gmail.com>
2024-02-20 17:07:56 +0300

    New changes

--- Dukatech/dukatech/settings.py
+++ Dukatech/dukatech/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Sokobeauty',
     'shop',
+    # 'rest_framework',
+    # 'mama_pesa',
 ]
 
 MIDDLEWARE = [
--- Dukatech/shop/admin.py
+++ Dukatech/shop/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import UserPaymentMethod
+from .models import UserPaymentMethod, Product, Category
 
 # Register your models here.
 admin.site.register(UserPaymentMethod)
+admin.site.register(Category)
+admin.site.register(Product)
